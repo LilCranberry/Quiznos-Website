cb44ddabc8a8f107a9bfe7eb737857753ed2a3bb
LilCranberry <chrisd5622@gmail.com>
2021-06-30 14:34:44 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+var number = 10;
+var string = "Subscribe to our mailing list!";
+var isRad = true;
+
+document.getElementById("box").innerHTML.addEventListener("click", function()
+{
+    alert("I got clicked");
+});
